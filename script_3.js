03a529d620fd5bbc029e3218059fd374a7fba836
Kseniya Sviridova <ksyu.sviridova.eger@mail.ru>
2019-11-04 20:27:08 +0300

    add changes

--- script_3.js
+++
@@ -1,8 +0,0 @@
-var canv = document.getElementById("canv_3"), // Select our canvas element
-    ctxt = canv.getContext("2d");
-
-
-var img = new Image();
-img.src = "rss-codejam-canvas/Sheets/image.png";
-
-ctxt.drawImage(img, 100, 100);
